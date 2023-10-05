eacf7ab3f664fa1669520211b7045da99b1c6f93
Ozomata-Sunday <141594784+Ozomata-Sunday@users.noreply.github.com>
2023-10-05 07:40:59 +0100

    added location

--- App.js
+++ App.js
@@ -2,17 +2,24 @@ import React, { useState, useEffect } from "react";
 import { View, ActivityIndicator, StyleSheet } from "react-native";
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from "./src/components/Tabs";
-import * as Location from 'expo-location'
+import * as Location from 'expo-location';
+import { WEATHER_API_KEY } from "@env"
 
 
 const App = () => {
   const [loading, setLoading] = useState(false)
   const [location, setLocation] = useState(null)
   const [error, setError] = useState(null)
+  const [weather, setWeather] = useState([])
 
+  const fetchWeatherData = async () => {
+    const res = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`)
+    const data = await Response.json()
+
+  }
   useEffect(() => {
     (async () => {
-      
+
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
         setErrorMsg('Permission to access location was denied');
@@ -24,14 +31,14 @@ const App = () => {
     })();
   }, []);
 
-  if(location){
-    console.log(location)
+  if (location) {
+    //console.log(location)
   }
 
-  if(loading) {
-    return(
-      <View style = {style.container}>
-        <ActivityIndicator size={'large'} color ={'blue'}/>
+  if (loading) {
+    return (
+      <View style={style.container}>
+        <ActivityIndicator size={'large'} color={'blue'} />
       </View>
     )
   }
@@ -39,7 +46,7 @@ const App = () => {
 
   return (
     <NavigationContainer>
-        <Tabs />
+      <Tabs />
     </NavigationContainer>
   )
 }
