cfe8eccc483028b261b9497239ea80c2bf182e8f
Ozomata-Sunday <141594784+Ozomata-Sunday@users.noreply.github.com>
2023-10-06 19:19:26 +0100

    getting data from openweather API

--- App.js
+++ App.js
@@ -2,38 +2,12 @@ import React, { useState, useEffect } from "react";
 import { View, ActivityIndicator, StyleSheet } from "react-native";
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from "./src/components/Tabs";
-import * as Location from 'expo-location';
-import { WEATHER_API_KEY } from "@env"
+import { useGetWeather } from "./src/Screens/Hooks/useGetWeather";
 
 
 const App = () => {
-  const [loading, setLoading] = useState(false)
-  const [location, setLocation] = useState(null)
-  const [error, setError] = useState(null)
-  const [weather, setWeather] = useState([])
-
-  const fetchWeatherData = async () => {
-    const res = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`)
-    const data = await Response.json()
-
-  }
-  useEffect(() => {
-    (async () => {
-
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-  }, []);
-
-  if (location) {
-    //console.log(location)
-  }
+  const [loading, error, weather] = useGetWeather()
+ console.log(loading, error, weather)
 
   if (loading) {
     return (
